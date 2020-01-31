5aa62bbc993b2254341e8549ec05e28c01d01a91
mbourinet <mathildebourinet@orange.fr>
2020-01-31 11:48:14 +0100

    update

--- Developpement/footer.php
+++ Developpement/footer.php
@@ -1,8 +1,8 @@
-
+<link rel="stylesheet" type="text/css" href="footer.css" media="all"/>
 <div class="footer">
     <p>Image</p>  
         <div>© <?php echo date("Y"); ?> Forum édité par Mathilde BOURINET , Gaspard BRISSET, Clément RAYSSIGUIER</div> 
-           
+          
    
  
         </div>   
