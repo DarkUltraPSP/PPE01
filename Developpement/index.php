1a4ae1854f4b6cae255e345efa5d15506e7eff0f
DarkUltraPSP <darkultrapsp.mc@gmail.com>
2020-01-31 11:17:39 +0100

    maj

--- Developpement/index.php
+++ Developpement/index.php
@@ -1,7 +1,19 @@
 <?php
 include ("header.php")
 ?>
-
+<div>
+	<p> Recherche sujet/auteur/message </p>
+</div>
+<div>
+	<table>
+		<tr>
+			<th> Topic </th> <th> Auteur </th>
+		</tr>
+		<tr>
+			<td></td><td></td>
+		</tr>
+	</table
+<div>
 <?php
 include ("footer.php")
 ?>
