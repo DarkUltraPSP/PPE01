3561e90546a76ef45764b469a3beb7f6fb584dfd
mbourinet <mathildebourinet@orange.fr>
2020-02-06 09:47:11 +0100

    Update

--- Developpement/header.php
+++ Developpement/header.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html lang="fr">
     <head>
+		
         <meta charset="utf-8" />
         <title>Forum PPE01</title>
-        <link rel="stylesheet" type="text/css" href="css/general.css" media="all"/>
+        <link rel="stylesheet" type="text/css" href="css/header.css" media="all"/>
 		<link rel="icon" type="image/logo.png" href="images/logo.png" />
     </head>
     <body>
-	<div>
+		
+		<div class = "entete">
+		</div>
--- Developpement/index.php
+++ Developpement/index.php
@@ -2,12 +2,12 @@
 <?php
 include_once("header.php")
 ?>
-<link rel="stylesheet" type="text/css" href="css/index.css" media="all"/>
+<link rel="stylesheet" type="text/css" href="css/index" media="all"/>
 
 <div class = "titre">
 	<p> Recherche sujet/auteur/message </p>
 </div>
-<div>
+<div class = "topic">
 	<table>
 		<tr>
 			<th> Topic </th> <th> Auteur </th>
