434040786d80ffa33f7cabf45259e8f6a5ea2007
mbourinet <mathildebourinet@orange.fr>
2020-02-06 09:05:47 +0100

    Update

--- Developpement/footer.php
+++ Developpement/footer.php
@@ -2,10 +2,8 @@
 <div class="footer">
     <p>Image</p>  
         <div>© <?php echo date("Y"); ?> Forum édité par Mathilde BOURINET , Gaspard BRISSET, Clément RAYSSIGUIER</div> 
-          
-   
- 
+		
         </div>   
-       
+		
     </body>
 </html>
