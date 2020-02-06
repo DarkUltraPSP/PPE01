24977e3d2b9137ee5f9f2e4f9904bb0a3bac81f4
GaspardBrisset <brissetgaspard@gmail.com>
2020-02-06 09:53:46 +0100

    maj footer

--- Developpement/footer.php
+++ Developpement/footer.php
@@ -1,20 +1,16 @@
-<<<<<<< HEAD
+
+<?php
+
+?>
 <link rel="stylesheet" type="text/css" href="css/footer.css" media="all"/>
 
        <div class="footer">
-      
-            <div>© <?php echo date("Y"); ?> Forum édité par Mathilde BOURINET , Clément RAYSSIGUIER , Gaspard BRISSET .</div> 
+           
+           <div><p>©  Forum édité par Mathilde BOURINET </p><p> Clément RAYSSIGUIER </p><p> Gaspard BRISSET </p></div> 
            
       
  
-=======
-<link rel="stylesheet" type="text/css" href="footer.css" media="all"/>
-<div class="footer">
-    <p>Image</p>  
-        <div>© <?php echo date("Y"); ?> Forum édité par Mathilde BOURINET , Gaspard BRISSET, Clément RAYSSIGUIER</div> 
-		
->>>>>>> 3561e90546a76ef45764b469a3beb7f6fb584dfd
         </div>   
-		
+       
     </body>
 </html>
