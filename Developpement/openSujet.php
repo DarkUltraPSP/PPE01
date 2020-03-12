aad9659106bdc3be6c6e2aacb27b9354b539cfb0
Nero <darkultrapsp.mc@gmail.com>
2020-03-12 23:39:14 +0100

    maj

--- Developpement/openSujet.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include_once 'include/header.php';
-
-    if (!empty($_POST["idSujet"]))
-    {
-        $idSujet = $_POST['idSujet'];
-        SujetManager::openSujet($idSujet);
-        header("Location: PageSujet.php?idSujet=$idSujet");
-    }
