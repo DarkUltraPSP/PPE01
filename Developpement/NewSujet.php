a13dfde9c6282975db20ffed1ee4a04d79bea957
Mathilde <mathildebourinet@orange.fr>
2020-03-15 23:02:26 +0100

    maj création sujet

--- Developpement/NewSujet.php
+++ Developpement/NewSujet.php
@@ -1,14 +1,18 @@
 <?php
 include_once 'include/header.php';
 ?>
-
-<form method="POST" action="NewSujetSender.php">
-    <input name="nomSujet" type="text" placeholder="Nom du Sujet"/>
-    <textarea name="content" placeholder="Ecrire un nouveau sujet"></textarea>
-    <input type="text" name="idUtilisateur" placeholder="idUtilisateur"/>
-    <input type="submit" value="Confirmer"/>
-</form>
-
+<link rel="stylesheet" type="text/css" href="css/NewSujet.css" media="all"/>
+<div class="bloc">
+    <p class="titre">Création d'un nouveau sujet :</p>
+    <div class="interieur">
+        <form method="POST" action="NewSujetSender.php">
+        <input class="barre" name="nomSujet" type="text" placeholder="Titre..."/>
+        <textarea class="barreTexte" name="content" placeholder="Ecrire un nouveau sujet"></textarea>
+        <input class="id" type="text" name="idUtilisateur" placeholder="idUtilisateur"/>
+        <input class="barre" type="submit" value="Confirmer"/>
+        </form>
+    </div>
+</div>
 <?php
 include_once 'include/footer.php';
 ?>
