b18a8d9bb7b5db802560379bc5a8cb12cda1561b
Mathilde <mathildebourinet@orange.fr>
2020-03-15 21:56:50 +0100

    maj connexion 2

--- Developpement/Connexion.php
+++ Developpement/Connexion.php
@@ -49,13 +49,18 @@
 <link rel="stylesheet" type="text/css" href="css/connexion.css" media="all"/>
 <div class="bloc">
     <p class="titre">Connexion :</p>
-    <form method="POST" action="Connexion.php">
+    <div class="interieur">
+        <form method="POST" action="Connexion.php">
+            <p class="titre2">Pseudo :</p>
+            <input class="barre" type ="text" name="pseudo" placeholder="Votre pseudo" required/>
+            <p class="titre2"> Mot de passe :</p>
+            <input class="barre" type ="text" name="password" placeholder="Votre mot de passe" required/>
+            </br>
+            </br>  
+            <input class="barre" type="submit" value="Connexion"/>
 
-        <input type ="text" name="pseudo" placeholder="Pseudo" required/>
-        <input type ="text" name="password" placeholder="Password" required/>
-        <input type="submit" value="Connexion"/>
-  
-    </form> 
+        </form> 
+    </div>
 </div>
 
 <?php
