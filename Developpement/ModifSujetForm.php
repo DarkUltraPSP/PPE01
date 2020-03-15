a00a48b1e5002af1b106c169fd9e608002b721f1
Mathilde <mathildebourinet@orange.fr>
2020-03-16 00:10:01 +0100

    css modif sujet

--- Developpement/ModifSujetForm.php
+++ Developpement/ModifSujetForm.php
@@ -8,14 +8,17 @@ if (!empty($_POST['idSujet']) && $_POST['content'] && $_POST['nomSujet'])
     $content = $_POST['content'];
 }
 ?>
-
-<form method="POST" action="ModifSujet.php">
-    <input type="text" value="<?php echo $nomSujet ?>" name="nomSujet" />
-    <textarea name="modifSujet"><?php echo $content ?></textarea>
-    <input type="hidden" value="<?php echo $idSujet?>" name="idSujet" />
-    <input type="submit" value="Modifier"/>
-</form>
-
+<div class="bloc">
+    <p class="titre">Modification du sujet...</p>
+    <div class="interieur">
+        <form method="POST" action="ModifSujet.php">
+            <input type="text" value="<?php echo $nomSujet ?>" name="nomSujet" />
+            <textarea name="modifSujet"><?php echo $content ?></textarea>
+            <input type="hidden" value="<?php echo $idSujet?>" name="idSujet" />
+            <input type="submit" value="Modifier"/>
+        </form>
+    </div>
+</div>
 <?php
 include_once 'include/footer.php';
 ?>
