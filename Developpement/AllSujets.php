921058f8cdbaa5f383199cdcc2ab025e2333f744
Mathilde <mathildebourinet@orange.fr>
2020-03-16 01:36:08 +0100

    AllSujets ++

--- Developpement/AllSujets.php
+++ Developpement/AllSujets.php
@@ -13,25 +13,40 @@ foreach ($sujets as $sujet)
 {
 ?>
 <div class="Sujet">
-    <form method='GET' action='PageSujet.php'>
-        <input type="hidden" value="<?php echo $sujet->getIdSujet(); ?>" name="idSujet"/>
-        <input type="submit" name="idSujetSubmit" class="NomSujet link" value="<?php echo $sujet->getNomSujet() ?>" />
-    </form>
-<?php
-echo "<div><h>Nombre de commentaires : ".SujetManager::countComs($sujet->getIdSujet())."</h></div>";
-    foreach ($users as $user)
-    {
-        if ($user->getIdUtilisateur() == $sujet->getIdUtilisateur())
-        {
-            echo "<h>Ecrit par : ".$user->getPseudo()."</h>";
-        }
-    }
-    echo "<div class='Date'> <h>Publié le : ".$sujet->getDateSujet()."</h></div>";
-    ?>
-    <form class="new" method="POST" action="SupprSujet.php">
-        <input type="hidden" value="<?php echo $sujet->getIdSujet(); ?>" name="idSujet"/>
-        <input type="submit" value="Supprimer" />
-    </form>
+    
+        <form class="titre" method='GET' action='PageSujet.php'>
+            <input type="hidden" value="<?php echo $sujet->getIdSujet(); ?>" name="idSujet"/>
+            <input type="submit" name="idSujetSubmit" class="NomSujet link" value="<?php echo $sujet->getNomSujet() ?>" />
+        </form>
+        <div class="bloc">
+            <div class="blocGauche">
+                <?php
+                echo "<div><h>Nombre de commentaires : ".SujetManager::countComs($sujet->getIdSujet())."</h></div>";
+                foreach ($users as $user)
+                {
+                    if ($user->getIdUtilisateur() == $sujet->getIdUtilisateur())
+                    {
+                        echo "<h>Ecrit par : ".$user->getPseudo()."</h>";
+                    }
+                }
+                echo "<div class='Date'> <h>Publié le : ".$sujet->getDateSujet()."</h></div>";
+                ?>
+            </div>
+            <div class="blocDroit">
+                //<?php
+//                if ($_SESSION['idAdmin']==1)
+//                {
+//                ?>
+                    <form class="new" method="POST" action="SupprSujet.php">
+                    <input type="hidden" value="<?php echo $sujet->getIdSujet(); ?>" name="idSujet"/>
+                    <input type="submit" value="Supprimer" />
+                    </form>
+                //<?php        
+//                }
+                
+//                ?>
+            </div>
+        </div>
 </div>
 <?php
 }   
