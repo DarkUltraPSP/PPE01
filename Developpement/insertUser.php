63df906f742ed5c1f40d93d27e163008a04cfcc6
Nero <darkultrapsp.mc@gmail.com>
2020-03-16 23:16:56 +0100

    Verif pseudo inscription

--- Developpement/insertUser.php
+++ Developpement/insertUser.php
@@ -1,7 +1,27 @@
 <?php
 include_once 'include/header.php';
 
-if (!empty($_POST['pseudo']) && !empty($_POST['mail']) && !empty($_POST['password']) && !empty($_POST['dateNaissance']) && !empty($_POST['sexe']) && !empty($_POST['pdp']))
+if (!empty($_POST['pseudo']))
+{
+    $pseudo = $_POST['pseudo'];
+    $mail = $_POST['mail'];
+}
+
+function testPseudo($pseudo, $mail)
+{
+    $users = UsersManager::findAllUsers();
+    $testPseudo = true;
+    foreach ($users as $user)
+    {
+        if ($pseudo == $user->getPseudo() || $mail == $user->getMail())
+        {
+            $testPseudo = false;
+        }
+    }
+    return $testPseudo;
+}
+
+if (!empty($_POST['pseudo']) && !empty($_POST['mail']) && !empty($_POST['password']) && !empty($_POST['dateNaissance']) && !empty($_POST['sexe']) && !empty($_POST['pdp']) && testPseudo($_POST['pseudo'],$_POST['mail']) == true)
 {
     $user = new User();
     $user->setPseudo($_POST['pseudo']);
@@ -9,6 +29,7 @@ if (!empty($_POST['pseudo']) && !empty($_POST['mail']) && !empty($_POST['passwor
     $user->setMotDePasse($_POST['password']);
     $user->setDateNaissance($_POST['dateNaissance']);
     $user->setSexe($_POST['sexe']);
+    
     if ($_POST['sexe'] == "Homme")
     {
         $user->setCheminPhoto("UserImage/user.png");
@@ -23,4 +44,14 @@ if (!empty($_POST['pseudo']) && !empty($_POST['mail']) && !empty($_POST['passwor
     }
     UsersManager::insertUser($user);
 }
+else 
+{
+    echo "<p>Ce nom d'utilisateur est deja pris. </p>";
+    echo "<p> Vous allez être redirigé vers la page d'inscription dans 3 secondes. </p>";
+    ?><META HTTP-EQUIV="Refresh" CONTENT="3; URL=Connexion.php"><?php
+    header("Location: Connexion.php");
+    exit;
+
+}
+
 header("Location: Connexion.php");
