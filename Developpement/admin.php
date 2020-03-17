66d2407d90dac3cab86c2eab775a0ddcd380885a
Nero <darkultrapsp.mc@gmail.com>
2020-03-18 00:26:00 +0100

    css

--- Developpement/admin.php
+++ Developpement/admin.php
@@ -5,11 +5,25 @@ if ($_SESSION['isAdmin']==1)
 {
     foreach($users as $user)
     {
+        ?>
+<table>
+    <tr>
+        
+    </tr>
+    <td>
+        
+    </td>
+</table>
+
+
+        echo '<pre>';
         echo $user->getPseudo();
         echo $user->getMail();
         echo $user->getSexe();
         echo $user->getDateNaissance();
         echo $user->getDateInscription();
+        echo "</pre>";
+        <?php
     }
 }
 else
